7207b82da82dd125c2028a8a2434957a6ca9e7ff
Noor-Hashem77 <noor.hashem@applyboard.com>
2022-04-07 00:03:35 -0400

    Forgot to commit few things

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
-app.get("/", (req, res, err) => {
+app.post("/api/topten", (req, res, err) => {
   // in here I want to send an axios request to a flight website and do the logic to get the cheapest flight and its information
   // in order to send back to the fonrt end to display
+
+  const { leavingFrom, goingTo, departDate, returnDate } = req.body;
+  res.send([leavingFrom, goingTo, departDate, returnDate]);
 });
 
 app.listen(3001, () => {
