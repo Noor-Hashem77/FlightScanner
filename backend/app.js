2f4e82e399336be5ce7090e703a4656ea4bba917
Noor-Hashem77 <noor.hashem@applyboard.com>
2022-04-08 02:43:56 -0400

    did not do much..

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.post("/api/topten", (req, res, err) => {
+app.post("/api/topten", async (req, res, err) => {
   // in here I want to send an axios request to a flight website and do the logic to get the cheapest flight and its information
   // in order to send back to the fonrt end to display
 
